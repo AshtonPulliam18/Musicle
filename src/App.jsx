e02a8e90501533da9bd6d3dcb0a52403a5fab63c
Ashton Pulliam <139722909+AshtonPulliam18@users.noreply.github.com>
2024-05-28 17:08:39 -0500

    Authorization Checkpoint
    Able to get token, but playback still under works.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,55 @@ import Musicboard from './components/Musicboard';
 import GuessEntry from './components/GuessEntry.jsx';
 import Musicbar from './components/Musicbar.jsx';
 import Logo from './components/Logo.jsx';
+import Authenticator from './components/Authenticator.jsx';
 import record from "./assets/record.png";
+import axios from "axios"
+import { usePlaybackState, useSpotifyPlayer } from "react-spotify-web-playback-sdk";
 
 const App = () => {
     const [guesses, setGuesses] = useState([]);
     const [progress, setProgress] = useState(6);
     const [target, setTarget] = useState(6);
     const [playing, setPlaying] = useState(false);
-
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [token, setToken] = useState("");
+    
     const progressRef = useRef(progress);
-
     progressRef.current = progress;
 
+    const client_id = "f13a11c782834762976c38298c0571e7";
+    const client_secret = "22e12b8aebcd4479906de80c65c6e14b";
+    const auth_endpoint = "https://accounts.spotify.com/authorize";
+    const redirect = "http://localhost:5173/callback";
+    const scopes ="streaming user-read-email user-read-private user-modify-playback-state"
+    
+    
+    const handleAuthenticated = () => {
+        window.open(`${auth_endpoint}?client_id=${client_id}&redirect_uri=${redirect}&response_type=token&scope=${scopes}`);
+    }
+    
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
+        
+        
+        if (!token & hash) {
+            token = hash.substring(1).split("&").find(ele => ele.startsWith("access_token")).split("=")[1];
+            
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+            setIsAuthenticated(true);
+        }
+        else if (token) {
+            window.location.hash = "";
+            setIsAuthenticated(true);
+        }
+        
+        setToken(token);
+    })
+
+    
+    
     let numSkips = 5;
 
     const handleSkip = (skipped) => {
@@ -41,10 +78,10 @@ const App = () => {
         }
     };
 
-    const handlePlay = () => {
-        setTarget(progress);
-        setProgress(0);
-        setPlaying(true);
+    const handlePlay = async () => {
+            setTarget(progress);
+            setProgress(0);
+            setPlaying(true);
     };
 
     useEffect(() => {
@@ -102,7 +139,7 @@ const App = () => {
 
     return (
         <div className="h-screen w-screen bg-paleYellow grid grid-cols-12 grid-rows-10 overflow-hidden">
-
+            
             {/* Logo/title */}
             <div className="row-span-3 col-span-5 grid grid-rows-12">
                 <Logo/>
@@ -110,15 +147,16 @@ const App = () => {
                 <div className="bg-lightOrange row-start-8 w-[50%] h-[50%] ml-[3%] rounded-md"/>
                 <div className="bg-darkOrange row-start-9 w-[80%] h-[50%] ml-[3%] rounded-md"/>
             </div>
-
-            {/* Musicbar */}
+            
+            <Authenticator onAuthenticate={handleAuthenticated} isEnabled={!isAuthenticated}/>
+             {/* Musicbar */}
             <div className="row-start-5 row-span-2 col-span-7">
                 <Musicbar progress={progress} />
             </div>
 
             {/* Play Button */}
             <div className="row-start-7 row-span-1 col-span-7 relative">
-                <img onClick={handlePlay} src={record} className="w-[10%] h-[100%] ml-[45%] cursor-pointer" />
+                <img onClick={handlePlay} src={record} className="w-[9%] h-[100%] ml-[45%] cursor-pointer" />
             </div>
 
             {/* Guess Entry */}
---
+++ src/components/Authenticator.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Authenticator = ({onAuthenticate, isEnabled}) => {
+    if (!isEnabled) return null;
+
+    
+
+    
+    return (
+        
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
+            <div className="bg-lightPink p-8 rounded-lg text-center text-darkYellow font-sand shadow-lg">
+                <h2 className="mb-6 text-xl">Welcome to Musicle!</h2>
+                <button
+                    className="bg-darkYellow text-darkPink font-sand py-2 px-6 rounded"
+                    onClick={onAuthenticate}
+                    
+                >
+                    Play
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Authenticator;
